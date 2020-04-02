a7ba09ab67e08fb6c723d67f2c977355bf0faade
gituajames <gitua85@gmail.com>
2020-04-02 09:51:20 +0300

    comments

--- scrap.py
+++ scrap.py
@@ -3,14 +3,19 @@ import geopandas as gpd
 import requests
 from bs4 import BeautifulSoup as b
 
+# a procedure function specific to table at 'https://ncov2019.live/'
 def table():
+    # make a request to target website
     r = requests.get('https://ncov2019.live/')
     r.status_code
     
+    # parse the text
     soup = b(r.text, 'lxml')
-    
+    # and locate the target table
     global_ = soup.find('table', attrs={'id': 'sortable_table_global'})
     
+    # extract all table row to a list of list 
+    # and make a dataframe from it
     table_row = global_.find_all('tr')
     all_rows = []
     for tr in table_row:
@@ -21,10 +26,15 @@ def table():
     df = pd.DataFrame(all_rows, columns=['country', 'confirmed', 'changes1', 'changes1(%)', 'deceased', 
                                      'changes2', 'changes2(%)', 'recovered', 'serious'])
     
+    # drop any row with all None nalues
+    # and unnecessary columns
     df.dropna(inplace=True)
     df.drop(index=[1], inplace=True)
     df.drop(columns=['changes1', 'changes1(%)', 'changes2', 'changes2(%)'], inplace=True)
     
+
+    # strip all the white spaces, character and puntuation marks
+    # such as commas
     copy_df = df.copy()
     copy_df['country'] = copy_df['country'].str.replace('★', '')
     copy_df['country'] = copy_df['country'].str.strip()
@@ -33,6 +43,8 @@ def table():
     copy_df['recovered'] = copy_df['recovered'].str.replace(',', '')
     copy_df['serious'] = copy_df['serious'].str.replace(',', '')
     
+    # convert necessary columns to int and float datatypes
+    # for computations
     copy_df['confirmed'] = copy_df['confirmed'].astype(int)
     copy_df['deceased'] = pd.to_numeric(copy_df['deceased'])
     copy_df['recovered'] = pd.to_numeric(copy_df['recovered'])
